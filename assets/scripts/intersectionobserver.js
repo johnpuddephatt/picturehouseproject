be65478e8cac251fa84278067d2405c274905031
John Puddephatt <john@jdp.org.uk>
2017-10-26 21:36:07 +0100

    more IO

--- assets/scripts/intersectionobserver.js
+++ assets/scripts/intersectionobserver.js
@@ -1,5 +1,5 @@
 var options = {
-  threshold: [0.1, 0.9],
+  threshold: [0.9],
   rootMargin: '0px'
 
 };
@@ -8,8 +8,11 @@ var images = document.querySelectorAll('.aim-image .bw');
 
 var callback =  function(entries, observer) {
   entries.forEach(entry => {
-    if(entry.intersectionRatio > 0) {
-      entry.target.style.opacity = 1 - Math.round( entry.intersectionRatio * 10 ) / 10;
+    if(entry.intersectionRatio > 0.9) {
+      entry.target.style.opacity = 1;
+    }
+    else {
+      entry.target.style.opacity = 0;
     }
   });
 }
