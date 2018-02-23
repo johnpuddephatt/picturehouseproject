684810eae4a53594dbbf059dce350d261c7cd667
John Puddephatt <john@jdp.org.uk>
2018-02-23 11:49:55 +0000

    blur images before lazyload 11

--- assets/scripts/intersectionobserver.js
+++ assets/scripts/intersectionobserver.js
@@ -9,10 +9,10 @@ var images = document.querySelectorAll('.aim-image .bw');
 var callback =  function(entries, observer) {
   entries.forEach(entry => {
     if(entry.intersectionRatio >= 0.95) {
-      entry.target.style.opacity = 0;
+      entry.target.parentNode.classList.add('in-view');
     }
     else {
-      entry.target.style.opacity = 1;
+      entry.target.parentNode.classList.remove('in-view');
     }
   });
 }
